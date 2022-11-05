b8cd40eb87b8a560a692fa275ea2ec8bb7a10bf5
alons <alons@DESKTOP-HCS5CNU>
2022-11-05 17:30:24 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoEcoFit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoEcoFit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
